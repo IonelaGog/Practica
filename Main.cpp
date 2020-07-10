97fec25150ce712864b859e46b50ea79be33b68d
Ionela Gog <gog_ionela@yahoo.com>
2020-07-10 09:25:35 +0300

    Initial commit

---
+++ Main.cpp
@@ -0,0 +1,20 @@
+#include "Vector.hpp"
+#include "List.hpp"
+#include "ListNode.hpp"
+#include <cstdlib>
+
+int main(){
+    Vector vec;
+    std::size_t end = vec.getCapacity();
+
+    for (std::size_t idx = 0; idx < end; ++idx){
+        vec.setElement(idx, std::rand());
+    }
+
+    int sum = 0;
+
+    for (std::size_t idx = 0; idx < end; ++idx){
+        sum += vec.getElement(idx);
+    }
+   return sum;
+}
